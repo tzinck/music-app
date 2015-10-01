a4cfe904d8a4e528e0d922a420203cdd6cf202ae
Tanner <norcorider11@gmail.com>
2015-09-30 23:52:53 -0700

    Have very basic functionality working. Can add songs to the DB, print contents of DB, and delete everything in DB. Using very crude while and if statements to take in command. Need to fix that.

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from databasecalls import *
+
+#File to contain the bulk of the application (for now, in the preliminary stages)
+
+startup()
+
+print('Done Startup')
+
+#This is a very messy way to handle taking user input, need to rework this
+while(1):
+	action = input('Enter a Command: ')
+	if(action == 'add'):
+		#handle adding
+		name = input('Enter song name: ')
+		insert(name)
+	if(action == 'printall'):
+		#handle printing
+		printall()
+	if(action == 'deleteall'):
+		#handle deletion
+		deleteall()
---
+++ databasecalls.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+#File to contain all calls to the database
+
+db = sqlite3.connect('music-db')
+cursor = db.cursor()
+
+def startup():
+	db = sqlite3.connect('music-db')
+	cursor = db.cursor()
+	cursor.execute('''
+		CREATE TABLE IF NOT EXISTS songs(id INTEGER PRIMARY KEY, name TEXT)
+			''')
+	db.commit()
+	print('Connected to DB')
+
+def insert(songname):
+	cursor.execute('''INSERT INTO songs(name) VALUES (?)''', (songname,))
+	db.commit()
+	print('Inserted song: ' + songname)
+
+def printall():
+	cursor.execute('''SELECT name FROM songs''')
+	all_rows = cursor.fetchall()
+	for row in all_rows:
+		print('{0} '.format(row[0]))	
+
+def deleteall():
+	cursor.execute('''DELETE FROM songs''')
+	db.commit()
+
+
+	
+
+	
+	
